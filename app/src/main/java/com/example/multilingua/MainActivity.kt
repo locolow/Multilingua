f078b49d2b25fcc7777a43f7a41f432d72d55946
Alberto Castro <albertocastro@ipvc.pt>
2022-04-20 20:34:34 +0100

    Initial commit

---
+++ app/src/main/java/com/example/multilingua/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.multilingua
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Toast.makeText(this,R.string.welcome, Toast.LENGTH_SHORT).show()
+    }
+}
